5fcd68bbc33f8b7b6d15e7230cc65864c8e8b7a0
jose.villarreal <quetzal1910@gmail.com>
2023-06-28 14:00:30 -0500

    List exercises

--- lista.py
+++ lista.py
@@ -45,3 +45,39 @@ l = n
 
 print("l:", l)
 print("n:", n)
+
+m = [-11, 2, -1, 4, 5, 6]
+
+# Crear una nueva lista que intercambie del lugar el primer elemento y el último
+
+# Extraer el último elemento de la lista m
+
+print(m[-1])
+print(m[-2])
+
+print(m[len(m) - 1])
+
+# Imprimir la lista m en órden inverso
+
+m.reverse()
+print("m", m)
+
+start = [3, 4]
+start.extend(m)
+
+# Para cada elemento en turno 'e' en la lista start
+# imprime(e)
+
+for e in start:
+    print(e)
+
+#funcion(a, b){
+# haz con f1 lo que quieras
+#}
+
+
+for e in range(0, len(start), 2):
+    print(start[e])
+
+
+# TODO: Extraer los elementos pares de start
